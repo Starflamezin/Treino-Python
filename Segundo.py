a912e7d40b6634f3a0a1fb8dd94f0b6536101869
Rafael Guilhem <53585761+Starflamezin@users.noreply.github.com>
2019-11-18 21:00:45 -0300

    Add files via upload

---
+++ Segundo.py
@@ -0,0 +1,8 @@
+a = int(input("Digite sua idade:\n"))#Pega a idade
+b = float(input("Digite seu peso:\n"))#Pega o peso
+c = float(input("Digite sua altura:\n"))#Pega a altura
+
+if a == 18 and b >= 65 and c >= 1.80:#condicional
+    print("Apto para entrar no exercito")
+else:#Se a condicional nao for cumprida 
+    ("Näo apto para entrar no exercito")
